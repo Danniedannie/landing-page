760797e9cca82d62e744b50f160d3fc72e7efa2b
Danniedannie <danielle.simone.lynch@gmail.com>
2019-12-01 14:25:05 +0000

    set Jquery for active class

--- JS/app.js
+++ JS/app.js
@@ -12,10 +12,44 @@ for (let i = 0; i < htmlContent.length; i++) {
     li.setAttribute('class', htmlContent[i].id);
     console.log(li)
     li.addEventListener('click', scrollToSection);
+    //console.log(li.className);
     var finalizedData = li.textContent += htmlContent[i].dataset.nav;
 
     //scrollTo helper function
-    function scrollToSection(x) {
+    function scrollToSection() {
         location.hash = htmlContent[i].id;
     }
-};
+
+    /* function addActiveClass() {
+        var activeclass = document.querySelectorAll('section');
+        for (var i = 0; i < activeclass.length; i++) {
+            activeclass[i].addEventListener('click', activateClass);
+        }
+
+        function activateClass(e) {
+            for (var i = 0; i < activeclass.length; i++) {
+                activeclass[i].classList.remove('your-active-class');
+            }
+            e.target.classList.add('your-active-class');
+            console.log(li)
+        }
+ */
+}
+
+{
+    /* <script
+    src = "https://code.jquery.com/jquery-3.4.1.js"
+    integrity = "sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+    crossorigin = "anonymous" > < /script>
+
+    $(document).on('click', 'ul li', function() {
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+
+
+    // if (current.className <= 0) { */
+}
+//     current.className = current.className.replace(" active", "");
+// }
+// console.log(current);
+// // Add the active class to the current/clicked button
