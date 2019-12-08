b6e244e021d6a090394f2cc27978f8a436d5dc8e
Danniedannie <danielle.simone.lynch@gmail.com>
2019-12-08 11:52:52 +0000

    add active class on scroll

--- JS/app.js
+++ JS/app.js
@@ -1,34 +1,3 @@
-// const sections = document.querySelectorAll('section');
-// const ul = document.createElement('ul');
-// document.getElementById('navbar__menu').appendChild(ul);
-// //loop through elements to grab the data-nav elements
-// for (let i = 0; i < sections.length; i++) {
-//     const section = sections[i];
-//     const li = document.createElement('li');
-//     li.setAttribute('id',section.dataset.nav);
-//     li.addEventListener('click', function clickHandler() {
-//         location.hash = section.id;
-//       const currentSection = document.querySelector(`[data-nav=${this.id}]`);
-//        // remove class from sibling sections
-//       const allSections = document.querySelectorAll('section');
-//       allSections.forEach((section) =>{
-//         console.log(section)
-//         section.classList.remove('active');
-//       })
-//        // add class to section
-//       currentSection.classList.add('active');
-//       const allLi = document.querySelectorAll('li');
-//       allLi.forEach((li) =>{
-//         console.log(li)
-//         li.classList.remove('active');
-//       })
-//         // remove class from sibling section links
-//       this.classList.add('active');
-//     });
-//     li.textContent = section.dataset.nav;
-//     ul.appendChild(li);
-// };
-
 const sections = document.querySelectorAll('section');
 const ul = document.createElement('ul');
 document.getElementById('navbar__list').appendChild(ul);
@@ -46,19 +15,31 @@ for (let i = 0; i < sections.length; i++) {    
         const allSections = document.querySelectorAll('section');
         const allSectionArray = Array.prototype.slice.call(allSections);
 
-        // remove class from sibling sections
-              
-        allSections.forEach((section) => {        
-            console.log(section)        
+        // remove class from sibling sections     
+        allSections.forEach((section) => {               
             section.classList.remove('active');      
         })       
 
-        // add class to section
-        currentSection.classList.add('active');
+        //checking if section is in viewport -- logic borrowed from https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+        var isInViewport = function(section) {
+            var bounding = currentSection.getBoundingClientRect();
+            return (
+                bounding.top >= 0 &&
+                bounding.left >= 0 &&
+                bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+        };
+
+        // on scroll add active class
+        window.addEventListener('scroll', function(event) {
+            if (isInViewport(currentSection)) {
+                currentSection.classList.add('active'); 
+            }
+        }, false);
 
         const allLi = ul.querySelectorAll('li');      
-        allLi.forEach((li) => {        
-                console.log(li)        
+        allLi.forEach((li) => {               
                 li.classList.remove('active');     
             })        
             // remove class from sibling section links
